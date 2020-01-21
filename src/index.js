1c82c138bc6bf6469816dc1bcd8926db4de14167
107124 <kredfox4@gmail.com>
2020-01-21 16:27:56 -0700

    fixed issues where the background was taking the 8-ball instead of a simple image of the 8-ball.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,45 @@ import "./styles.css";
 
 import Ball from "./images/ball.png";
 
+let showIt = false;
+
+const handleSubmit = () => {
+  const answers = [
+    "\nAs I see it, yes.",
+    "\nI'd better not tell you now...",
+    "\nUm... seriously?",
+    "\nDon’t count on it.",
+    "\nIt is certain.",
+    "\nMost likely.",
+    "\nMeh.",
+    "\nMaybe leave that one up to Google.",
+    "\nI wouldn't say so.",
+    "\nMy sources say no.",
+    "\nThe outlook is not so good.",
+    "\nThe outlook is good.",
+    "\nSigns point to yes.",
+    "\nVery doubtful.",
+    "\nWithout a doubt.",
+    "\nYes.",
+    "\nYes – definitely.",
+    "\nYou may rely on it."
+  ];
+  console.log(answers);
+  showIt = true;
+  return answers[Math.floor(Math.random() * answers.length)];
+};
+
 function App() {
   return (
     <div className="App">
-      <img src={Ball} alt="logo" />
-      <input placeholder="Enter a 'Yes' or 'No' question" />
-      <button className="button">Fortell</button>
+      <form onSubmit={handleSubmit}>
+        <img src={Ball} alt="logo" />
+        <input placeholder="Enter a 'Yes' or 'No' question" />
+        <button className="button" type="submit">
+          Fortell
+        </button>
+      </form>
+      <p>{handleSubmit()}</p>
     </div>
   );
 }
